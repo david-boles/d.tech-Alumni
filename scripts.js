65475b120e1aa0f1059b6f0e2c6711eafc1b9a93
David Boles <me@davidbol.es>
2018-04-24 11:19:30 -0700

    Started work on a getData function.

--- scripts.js
+++ scripts.js
@@ -1,4 +1,20 @@
+//TODO Implement maps: https://developers.google.com/maps/documentation/javascript/marker-clustering
+
 //alert("sometext");
 setTimeout(function(){
   document.getElementById('paragraph').innerText="somenewtext";
 },5000);
+
+//Attempt to retrieve and parse the database from GitHub. Arguments are functions that get passed the parsed JSON, the XMLHttpRequest, or _ respectively.
+function getData(onSuccess, onGetFail, onParseFail) {
+  var xmlHttp = new XMLHttpRequest();
+  xmlHttp.onreadystatechange = function() { 
+    if (xmlHttp.readyState == 4 && xmlHttp.status == 200) {
+      callback(xmlHttp.responseText);
+    }else {
+      onGetFail(xmlHttp);
+    }
+  }
+  xmlHttp.open("GET", theUrl, true); // true for asynchronous 
+  xmlHttp.send(null);
+}

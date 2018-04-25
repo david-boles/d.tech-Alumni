7b1c297d32098962760e0b35ec7d941beb1e8ff0
jephf <38699821+jephf@users.noreply.github.com>
2018-04-25 10:26:36 -0700

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -18,6 +18,7 @@ function initMap() {
     }
    var marker = new google.maps.Marker({
           position: BrownUniversity,
+          label: "Brown University",
           map: map
         });
 }

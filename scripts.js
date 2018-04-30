4208160f65dc3e07f20f747cdec76699041e648b
David Boles <me@davidbol.es>
2018-04-30 11:14:56 -0700

    \n not working

--- scripts.js
+++ scripts.js
@@ -28,7 +28,7 @@ function addInfoWindow(marker, place) {
   var text = place.name;
   for(var i in place.alumni) {
     var alumn = place.alumni[i];
-    text += '\n- ' + alumn.name + ' \'' + alumn.year;
+    text += '<br>- ' + alumn.name + ' \'' + alumn.year;
   }
 
   var infoWindow = new google.maps.InfoWindow({

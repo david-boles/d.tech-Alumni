a7248d69cad50f8583657f01822b2052a6addd3e
jephf <38699821+jephf@users.noreply.github.com>
2018-04-30 10:22:16 -0700

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -4,7 +4,7 @@ function initMap() {
   
   //getData(function(Data){
   var map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 8,
+    zoom: 1,
     center: {lat: 37.533034, lng: -122.264452}
   });
   
